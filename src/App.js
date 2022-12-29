ab20ae749db7b475a7821cd3b5e083189d416a63
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-12-29 13:11:20 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Portfolio from './components/Portfolio';
-import About from './components/About';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import Header from './components/Header/index';
+import Footer from './components/Footer/index';
+import Portfolio from './components/Portfolio/index';
+import About from './components/About/index';
+import Contact from './components/Contact/index';
+import Resume from './components/Resume/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -21,10 +21,10 @@ function App() {
               path="/Brad_Hambrick_React_Portfolio/"
               element={<About />}
             />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
+            <Route exact path="/portfolio/" element={<Portfolio />} />
+            <Route exact path="/about/" element={<About />} />
+            <Route exact path="/contact/" element={<Contact />} />
+            <Route exact path="/resume/" element={<Resume />} />
           </Routes>
         </div>
       </BrowserRouter>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,10 +15,10 @@ class Header extends Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/about">About Me</Nav.Link>
-                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="/resume">Resume</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
+                <Nav.Link href="/about/">About Me</Nav.Link>
+                <Nav.Link href="/portfolio/">Portfolio</Nav.Link>
+                <Nav.Link href="/resume/">Resume</Nav.Link>
+                <Nav.Link href="/contact/">Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
