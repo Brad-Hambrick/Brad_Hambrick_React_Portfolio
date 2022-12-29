7ecc2113f90a1cc425783a3d546311920e740035
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-12-29 12:22:43 -0600

    debugging routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
 
       <Footer />
     </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Portfolio from '../Portfolio';
-import About from '../About';
-import Contact from '../Contact';
-import Resume from '../Resume';
 
 class Header extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="header">
-          <Navbar bg="light" expand="lg">
-            <Container>
-              <Navbar.Brand href="#home">
-                Brad Hambrick's Full Stack Web Development Portfolio
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="/about">About Me</Nav.Link>
-                  <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-                  <Nav.Link href="/resume">Resume</Nav.Link>
-                  <Nav.Link href="/contact">Contact</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </div>
-
-        <div className="content">
-          <Routes>
-            <Route exact path="/" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="header">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">
+              Brad Hambrick's Full Stack Web Development Portfolio
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/about">About Me</Nav.Link>
+                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="/resume">Resume</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     );
   }
 }
