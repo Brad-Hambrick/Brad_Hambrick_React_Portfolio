910014a32a3c287de5858ce392a5544c76583589
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2022-12-29 21:30:39 -0600

    debug more

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,16 +10,20 @@ class Header extends Component {
       <div className="header">
         <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand href="/">
+            <Navbar.Brand href="#">
               Brad Hambrick's Full Stack Web Development Portfolio
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/about">About Me</Nav.Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/portfolio">Portfolio</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/contact">Contact</Link>
+                {/* <Nav.Link href="/about">About Me</Nav.Link>
                 <Nav.Link href="/portfolio">Portfolio</Nav.Link>
                 <Nav.Link href="/resume">Resume</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link> */}
               </Nav>
             </Navbar.Collapse>
           </Container>
